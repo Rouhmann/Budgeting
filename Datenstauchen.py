093ac1de6cb841beccc26f2006504988533c5378
Rouhmann <108574414+Rouhmann@users.noreply.github.com>
2022-07-24 18:27:20 +0200

    Update

--- Datenstauchen.py
+++ Datenstauchen.py
@@ -8,18 +8,12 @@ df = pd.read_csv("Umsaetze.csv", sep=";")
 
 df.rename(columns={"Name Zahlungsbeteiligter": "Zahlungsbeteiligter", "Saldo nach Buchung": "Saldo"}, inplace = True)
 #Dropping sensitive and non-useful data
-df.drop("IBAN Auftragskonto", axis = 1, inplace = True)
-df.drop("Bezeichnung Auftragskonto", axis = 1, inplace = True)
-df.drop("BIC Auftragskonto", axis = 1, inplace = True)
-df.drop("IBAN Zahlungsbeteiligter", axis = 1, inplace = True)
-df.drop("Bankname Auftragskonto", axis = 1, inplace = True)
-df.drop("Valutadatum", axis = 1, inplace = True)
-df.drop("BIC (SWIFT-Code) Zahlungsbeteiligter", axis = 1, inplace = True)
-df.drop("Bemerkung", axis = 1, inplace = True)
-df.drop("Kategorie", axis = 1, inplace = True)
-df.drop("Steuerrelevant", axis = 1, inplace = True)
-df.drop("Glaeubiger ID", axis = 1, inplace = True)
-df.drop("Mandatsreferenz", axis = 1, inplace = True)
+
+col_drop = ["IBAN Auftragskonto",  "Bezeichnung Auftragskonto", "BIC Auftragskonto", "IBAN Zahlungsbeteiligter", "Bankname Auftragskonto", "Valutadatum",
+"BIC (SWIFT-Code) Zahlungsbeteiligter", "Bemerkung", "Kategorie","Steuerrelevant", "Glaeubiger ID", "Mandatsreferenz"]
+for col in col_drop:
+    df.drop(col, axis = 1, inplace = True)
+
 df = df[df.Zahlungsbeteiligter.notnull()]
 
 #Output to .csv
